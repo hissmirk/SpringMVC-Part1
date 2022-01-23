318fed698c5f845e67dcf47f62815382c618e419
Lowheads <false9@kakao.com>
2022-01-23 22:26:02 +0900

    first commit

---
+++ src/main/java/hello/springmvc/InflearnSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringmvcApplication.class, args);
+    }
+
+}
